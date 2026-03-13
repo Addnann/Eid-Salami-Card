d6cd62ddfe2c1e9cb790f017e76632d1272fbc2d
Adnan Dewan <adnandewan77@gmail.com>
2026-03-13 21:53:39 +0600

    Updated the code

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 document.getElementById('copyNum').addEventListener('click', function() {
     const num = this.innerText;
+
     navigator.clipboard.writeText(num).then(() => {
-        alert('The number has been copied: ' + num);
+        alert('Number copied! Now paste it in the bKash app.');
+
+        setTimeout(function() {
+            window.location.href = "bkash://"; 
+            
+            setTimeout(function() {
+                if (document.hasFocus()) {
+                    window.location.href = "https://www.bkash.com/app/";
+                }
+            }, 2000);
+        }, 500);
+
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
     });
 });
