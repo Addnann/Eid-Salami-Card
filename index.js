5649d1c9b3d3e2941f8e06918758e6a21061382d
Adnan Dewan <adnandewan77@gmail.com>
2026-03-19 20:42:43 +0600

    Updated the code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-document.getElementById('copyNum').addEventListener('click', function() {
+document.getElementById('copyNum').addEventListener('click', function () {
     const num = this.innerText;
 
-    navigator.clipboard.writeText(num).then(() => {
-        alert('Number copied! Opening bKash app...');
+    navigator.clipboard.writeText(num);
 
+    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isMobile) {
         window.location.href = "bkash://";
-
-        setTimeout(function() {
-            if (document.hasFocus()) {
-                window.location.href = "https://www.bkash.com/app/";
-            }
-        }, 2500);
-
-    }).catch(err => {
-        console.error('Error:', err);
-    });
+        
+        setTimeout(function () {
+            window.location.href = "https://www.bkash.com/app/";
+        }, 2000);
+    } else {
+        alert("Please open this on your mobile to use bKash app.");
+    }
 });
